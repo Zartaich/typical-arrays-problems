db6d990b21d97c2f01b313f4d9599a182319a060
Zartaich <zartaich@gmail.com>
2020-10-03 22:20:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    } else {
+        const arr = array.sort((a, b) => a - b);
+        return arr[0];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    } else {
+        const arr = array.sort((a, b) => a - b);
+        return arr[arr.length - 1];
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    } else {
+        return (
+            array.reduce(
+                (accumulator, currentValue) => accumulator + currentValue
+            ) / array.length
+        );
+    }
+};
